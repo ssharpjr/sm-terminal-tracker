63b695378e04a58d8d3f19baa80230273878f39e
Stacey Sharp <ssharpjr@gmail.com>
2020-10-13 23:25:06 -0400

    updated comments

--- src/scripts/SMTT/Initialization.lua
+++ src/scripts/SMTT/Initialization.lua
@@ -1,4 +1,13 @@
--- SMTT - Starmourn Terminal Tracker
+-------------------------------------------------------------------
+-- SMTT - Starmourn Terminal Tracker                             --
+--                                                               --
+-- A Mudlet Package for tracking hackable terminals in Starmourn --
+-- Game: Starmourn (http://www.starmourn.com)                    --
+-- Client: Mudlet (http://www.mudlet.org)                        --
+--                                                               --
+-- Package Author: Daikojun (github.com/ssharpjr)                --
+-- Source: (https://github.com/ssharpjr/sm-terminal-tracker)     --
+-------------------------------------------------------------------
 
 -- Initialize
 
